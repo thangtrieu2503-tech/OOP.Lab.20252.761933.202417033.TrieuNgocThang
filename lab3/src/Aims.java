8e8cfe62b237f4c77a4c4ccc6c99969d319895ec
ThangTrieu <thangtrieu2503@gmail.com>
2026-04-24 03:48:08 +0700

    Complete method overloading 2.1 + 2.2

---
+++ lab3/src/Aims.java
@@ -0,0 +1,39 @@
+public class Aims {
+    public static void main(String[] args) {
+        Cart anOrder = new Cart();
+
+        DigitalVideoDisc dvd1 = new DigitalVideoDisc(
+                "The Lion King", "Animation", "Roger Allens", 87, 19.95f);
+        anOrder.addDigitalVideoDisc(dvd1);
+
+        DigitalVideoDisc dvd2 = new DigitalVideoDisc(
+                "Star Wars", "Science Fiction", "George Lucas", 87, 24.95f);
+        anOrder.addDigitalVideoDisc(dvd2);
+
+        DigitalVideoDisc dvd3 = new DigitalVideoDisc(
+                "Aladin", "Animation", 18.99f);
+        anOrder.addDigitalVideoDisc(dvd3);
+
+        anOrder.displayCart();
+
+        System.out.print("Total cost is: ");
+        System.out.println(anOrder.totalCost());
+
+        DigitalVideoDisc foundDisc = anOrder.searchByTitle("Star Wars");
+        if (foundDisc != null) {
+            System.out.println("Found disc: " + foundDisc.getTitle());
+        } else {
+            System.out.println("Not found");
+        }
+
+        anOrder.sortByTitle();
+        anOrder.displayCart();
+
+        anOrder.sortByCost();
+        anOrder.displayCart();
+
+        anOrder.removeDigitalVideoDisc(dvd2);
+        System.out.print("Total cost is: ");
+        System.out.println(anOrder.totalCost());
+    }
+}
