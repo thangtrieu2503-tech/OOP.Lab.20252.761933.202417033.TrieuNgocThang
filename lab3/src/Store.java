af38442717a634913ce9273a7f558f55657fbf0b
ThangTrieu <thangtrieu2503@gmail.com>
2026-04-24 05:58:21 +0700

    Implement Store class

---
+++ lab3/src/Store.java
@@ -0,0 +1,33 @@
+public class Store {
+    public static final int MAX_ITEMS_IN_STORE = 100;
+    private DigitalVideoDisc[] itemsInStore = new DigitalVideoDisc[MAX_ITEMS_IN_STORE];
+    private int qtyInStore = 0;
+
+    public void addDVD(DigitalVideoDisc dvd) {
+        if (qtyInStore >= MAX_ITEMS_IN_STORE) {
+            System.out.println("The store is full");
+            return;
+        }
+
+        itemsInStore[qtyInStore] = dvd;
+        qtyInStore++;
+        System.out.println("DVD added to store successfully");
+    }
+
+    public void removeDVD(DigitalVideoDisc dvd) {
+        for (int i = 0; i < qtyInStore; i++) {
+            if (itemsInStore[i] == dvd) {
+                for (int j = i; j < qtyInStore - 1; j++) {
+                    itemsInStore[j] = itemsInStore[j + 1];
+                }
+
+                itemsInStore[qtyInStore - 1] = null;
+                qtyInStore--;
+                System.out.println("DVD removed from store successfully");
+                return;
+            }
+        }
+
+        System.out.println("DVD not found in store");
+    }
+}
